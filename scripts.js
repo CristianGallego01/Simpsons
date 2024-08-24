ed1251d20e988f148eefbcb5a7951eb5aa42a097
zowwii <cristiancmgallego@gmail.com>
2024-08-24 09:21:51 -0500

    create get an promise

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
 const url = "https://thesimpsonsquoteapi.glitch.me/quotes"
+const containerCards = document.querySelector(".cards-container")
+
+const getCharacter =async () =>{
+
+    const response = await fetch(url)
+    const data = response.json()
+    console.log(data)
+
+}
+
+fetch(url)
+.then(response => response.json())
+.then(data => console.log(data))
+
 
 const makeCharacter = (myCharacter) => {
 
@@ -15,5 +29,15 @@ const makeCharacter = (myCharacter) => {
     const nameCard = document.createElement("h3")
     nameCard.textContent = myCharacter.character
 
+    const quoteCard = document.createElement("p")
+    quoteCard.textContent = myCharacter.quote
+
+    card.appendChild(imgCard)
+    card.appendChild(cardContent)
+
+    cardContent.appendChild(nameCard)
+    cardContent.appendChild(quoteCards)
+
+    containerCards.appendChild(card)
 }
 
